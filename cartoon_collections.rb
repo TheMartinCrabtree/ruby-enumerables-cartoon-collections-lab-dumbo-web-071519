4bd3ff86d21b63266d109c4012dfafef9848d7f7
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-10 20:53:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,12 +8,9 @@ end
 
 def summon_captain_planet(array)
   
-  array.collect {} |name|
-    name = name.to_s.capitalize + "!"
-    
-  end
   
-  return return_array
+  
+  return array.collect { |name|  name = name.to_s.capitalize + "!"}
 end
 
 def long_planeteer_calls(array)
